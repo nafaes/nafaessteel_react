425676fc6475d3c9b310e1b5f21b774232541154
nafaes <Nafaes$123@>
2021-09-01 11:11:48 +0530

    Validations Added

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,11 +1,17 @@
 export const isInputNumber = (event, maxLength) => {
   let input = String.fromCharCode(event.which);
-  if (!/[0-9]/.test(input)) {
+  if (!new RegExp(/[0-9]/).test(input)) {
     event.preventDefault();
   }
   if (event.target.value === "0000") {
     event.preventDefault();
   }
+  if (event.target.value.length >= maxLength) {
+    event.preventDefault();
+  }
+};
+
+export const limitMaxlength = (event, maxLength) => {
   if (event.target.value.length >= maxLength) {
     event.preventDefault();
   }
