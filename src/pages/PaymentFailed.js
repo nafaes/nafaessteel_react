b9dc2fb71dae1b99b28cdcc49b84eece633f0982
nafaes <Nafaes$123@>
2021-10-05 17:12:01 +0530

    Payment success modified

--- src/pages/PaymentFailed.js
+++ src/pages/PaymentFailed.js
@@ -1,22 +1,17 @@
 import React from "react";
 import { useParams } from "react-router";
-import {
-  Grid,
-  makeStyles,
-  Typography,
-} from "@material-ui/core";
+import { Grid, makeStyles, Typography } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import failed from "../assets/img/failed.png";
 
-
 const useStyles = makeStyles((theme) => ({
   avatar: {
-    margin:" 0px auto",
+    margin: " 0px auto",
     width: "4em !important",
     height: "4em !important",
     position: "relative",
     top: "-2em",
-  }
+  },
 }));
 
 const PaymentFailed = () => {
@@ -25,94 +20,130 @@ const PaymentFailed = () => {
   const classes = useStyles();
 
   return (
-    <Grid container  style={{margin: "5em auto", width: "45%"}}>
+    <Grid container style={{ margin: "5em auto", width: "45%" }}>
       <Grid item container justifyContent="center">
-          <Grid item>
-          <img
-            src={failed}
-            alt="bg"
-            className={classes.avatar}
-          />
-          </Grid>        
+        <Grid item>
+          <img src={failed} alt="bg" className={classes.avatar} />
+        </Grid>
       </Grid>
-      <Grid container direction="column" justifyContent="center"
-          style={{ color: "white", borderRadius: "5px", textAlign: "center" , marginTop: "-1em" }} >   
-          <Grid item >
-              <Typography variant="h6" color="textPrimary" style={{color: "#0086af"}}>
-                  Payment Failed
-              </Typography>
-          </Grid>
-          
-     </Grid>
-     <Grid container style={{width: "75%" , margin: "0px 4em"}}>
-        <Grid item container   
-              style={{
-                padding: "0.5em",
-                marginTop: "2em",
-                backgroundColor: "rgba(211, 211, 211,0.9)"
-              }}>
-              <Grid item lg={6}>
-                <Typography  variant="h6" color="textPrimary" style={{fontSize: "1.15rem"}}>
-                  {t("PaymentFailed.Status")}
-                </Typography>
-              </Grid>
-              <Grid item lg={6} style={{textAlign: "right"}}>
-                <Typography variant="subtitle1" color="textSecondary">
-                  {paymentId}
-                </Typography>
-              </Grid>    
-        </Grid>
-        <Grid item container   
-          style={{
-            padding: "0.5em",
-            backgroundColor: "rgba(211, 211, 211,0.7)"
-          }}>
-          <Grid item lg={6}>
-            <Typography  variant="h6" color="textPrimary" style={{fontSize: "1.15rem"}}>
-                {t("PaymentFailed.PaymentId")}           
-             </Typography>
-          </Grid>
-          <Grid item lg={6} style={{textAlign: "right"}}>
-          <Typography variant="subtitle1" color="textSecondary">
-              {status}
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        style={{
+          color: "white",
+          borderRadius: "5px",
+          textAlign: "center",
+          marginTop: "-1em",
+        }}
+      >
+        <Grid item>
+          <Typography
+            variant="h6"
+            color="textPrimary"
+            style={{ color: "#0086af" }}
+          >
+            Payment Failed
           </Typography>
-          </Grid>                
         </Grid>
-        <Grid item container   
+      </Grid>
+      <Grid container style={{ width: "75%", margin: "0px 4em" }}>
+        <Grid
+          item
+          container
           style={{
             padding: "0.5em",
-            backgroundColor: "rgba(211, 211, 211,0.9)"
-          }}>
-            <Grid item lg={6}>
-                <Typography  variant="h6" color="textPrimary" style={{fontSize: "1.15rem"}}>
-                {t("PaymentFailed.ReferenceNumber")}
-                </Typography>
-            </Grid>
-          <Grid item lg={6} style={{textAlign: "right"}}>
-              <Typography variant="subtitle1" color="textSecondary">
-              {referenceNo}
-              </Typography>
-          </Grid>    
-        </Grid>
-        <Grid item container   
-          style={{
-            padding: "0.5em",
-            backgroundColor: "rgba(211, 211, 211,0.9)"
-          }}>
+            marginTop: "2em",
+            backgroundColor: "rgba(211, 211, 211,0.9)",
+          }}
+        >
           <Grid item lg={6}>
-            <Typography component="h4" variant="h6" color="textPrimary" style={{fontSize: "1.15rem"}}>
-            {t("PaymentFailed.Amount")}
+            <Typography
+              variant="h6"
+              color="textPrimary"
+              style={{ fontSize: "1.15rem" }}
+            >
+              {t("PaymentFailed.Status")}
             </Typography>
           </Grid>
-          <Grid item lg={6} style={{textAlign: "right"}}>
+          <Grid item lg={6} style={{ textAlign: "right" }}>
             <Typography variant="subtitle1" color="textSecondary">
-               {amount}
+              {status}
             </Typography>
-          </Grid>    
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          style={{
+            padding: "0.5em",
+            backgroundColor: "rgba(211, 211, 211,0.7)",
+          }}
+        >
+          <Grid item lg={6}>
+            <Typography
+              variant="h6"
+              color="textPrimary"
+              style={{ fontSize: "1.15rem" }}
+            >
+              {t("PaymentFailed.PaymentId")}
+            </Typography>
+          </Grid>
+          <Grid item lg={6} style={{ textAlign: "right" }}>
+            <Typography variant="subtitle1" color="textSecondary">
+              {paymentId}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          style={{
+            padding: "0.5em",
+            backgroundColor: "rgba(211, 211, 211,0.9)",
+          }}
+        >
+          <Grid item lg={6}>
+            <Typography
+              variant="h6"
+              color="textPrimary"
+              style={{ fontSize: "1.15rem" }}
+            >
+              {t("PaymentFailed.ReferenceNumber")}
+            </Typography>
+          </Grid>
+          <Grid item lg={6} style={{ textAlign: "right" }}>
+            <Typography variant="subtitle1" color="textSecondary">
+              {referenceNo}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          style={{
+            padding: "0.5em",
+            backgroundColor: "rgba(211, 211, 211,0.9)",
+          }}
+        >
+          <Grid item lg={6}>
+            <Typography
+              component="h4"
+              variant="h6"
+              color="textPrimary"
+              style={{ fontSize: "1.15rem" }}
+            >
+              {t("PaymentFailed.Amount")}
+            </Typography>
+          </Grid>
+          <Grid item lg={6} style={{ textAlign: "right" }}>
+            <Typography variant="subtitle1" color="textSecondary">
+              {amount}
+            </Typography>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
-   
-</Grid>
   );
 };
 
